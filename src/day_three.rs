c42d57c93fc0fda5424385c3e45b52ebd197d342
RyanRothweiler <ryanrothweiler@gmail.com>
2024-12-03 16:15:20 -0800

    d3 p2

--- src/day_three.rs
+++ src/day_three.rs
@@ -1,6 +1,7 @@
 use crate::Solution;
 
 const SAMPLE: &str = include_str!("../resources/day_three/day_three_sample.txt");
+const SAMPLE_TWO: &str = include_str!("../resources/day_three/day_three_sample_part_two.txt");
 
 pub fn build_solution() -> Solution {
     Solution {
@@ -12,12 +13,22 @@ pub fn build_solution() -> Solution {
 }
 
 fn part_one_solve(input: &str) -> f64 {
+    solve(input, false)
+}
+
+fn part_two_solve(input: &str) -> f64 {
+    solve(input, true)
+}
+
+fn solve(input: &str, handle_dont: bool) -> f64 {
     let mut ans: f64 = 0.0;
 
     let mut tokenizer = Tokenizer {
         data: input.chars().collect(),
         index: 0,
     };
+
+    let mut enabled = true;
 
     let mut tok = get_next_token(&mut tokenizer);
     while tok != Token::End {
@@ -40,8 +51,45 @@ fn part_one_solve(input: &str) -> f64 {
                             Token::Number(y),
                             Token::ClosedParen,
                         ) => {
-                            // println!("valid mul {x} x {y}");
-                            ans += (x * y) as f64;
+                            if handle_dont {
+                                if enabled {
+                                    // println!("valid mul {x} x {y}");
+                                    ans += (x * y) as f64;
+                                }
+                            } else {
+                                ans += (x * y) as f64;
+                            }
+                        }
+                        _ => {
+                            tokenizer.index = tok_start;
+                        }
+                    }
+                } else if s.ends_with("do") {
+                    let tok_start = tokenizer.index;
+
+                    let open_paren = get_next_token(&mut tokenizer);
+                    let close_paren = get_next_token(&mut tokenizer);
+
+                    match (open_paren, close_paren) {
+                        (Token::OpenParen, Token::ClosedParen) => {
+                            // println!("DO");
+                            enabled = true;
+                        }
+                        _ => {
+                            tokenizer.index = tok_start;
+                        }
+                    }
+                } else if s.ends_with("don't") {
+                    // enabled = false;
+                    let tok_start = tokenizer.index;
+
+                    let open_paren = get_next_token(&mut tokenizer);
+                    let close_paren = get_next_token(&mut tokenizer);
+
+                    match (open_paren, close_paren) {
+                        (Token::OpenParen, Token::ClosedParen) => {
+                            // println!("DON'T");
+                            enabled = false;
                         }
                         _ => {
                             tokenizer.index = tok_start;
@@ -58,14 +106,9 @@ fn part_one_solve(input: &str) -> f64 {
     ans
 }
 
-fn part_two_solve(input: &str) -> f64 {
-    0.0
-}
-
 #[derive(Debug, Eq, PartialEq)]
 enum Token {
     Word(String),
-    // Mul(String),
     Number(i32),
 
     OpenParen,
@@ -142,9 +185,22 @@ mod tests {
     }
 
     #[test]
+    fn part_two_sample() {
+        let ans = super::part_two_solve(super::SAMPLE_TWO);
+        assert_eq!(ans, 48.0);
+    }
+
+    #[test]
     fn part_one() {
         let sol = super::build_solution();
         let ans = sol.run_part_one();
         assert_eq!(ans, 183669043.0);
+    }
+
+    #[test]
+    fn part_two() {
+        let sol = super::build_solution();
+        let ans = sol.run_part_two();
+        assert_eq!(ans, 59097164.0);
     }
 }
